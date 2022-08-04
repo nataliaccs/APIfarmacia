a40d8ab878e6fc143033f6c80038feb46baf2a73
NatáliaCosta <naty1993ccs@gmail.com>
2022-08-04 09:54:04 -0300

    version1

---
+++ apifarmacia/src/main/java/com/farmabig/apifarmacia/ApiFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.farmabig.apifarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFarmaciaApplication.class, args);
+	}
+
+}
---
+++ apifarmacia/src/test/java/com/farmabig/apifarmacia/ApiFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmabig.apifarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
